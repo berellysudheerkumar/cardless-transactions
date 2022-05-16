76f338694ebfa678b3cb6676813d3233a5d7f0c5
Berelly <berelly.kumar@capgemini.com>
2022-05-16 11:13:03 +0530

    input update and a screen added

--- src/views/ATMScreen.vue
+++ src/views/ATMScreen.vue
@@ -1,10 +1,15 @@
 <template>
 <div>
     <h3>ATM Screen</h3>
+    <FirstScreen></FirstScreen>
 </div>
 </template>
 <script>
+import FirstScreen from '@/components/atm/FirstScreen'
 export default {
   name: 'ATMScreen',
+  components: {
+    FirstScreen,
+}
 }
 </script>
